a5ddf772097902c009649b876f075000686f725a
ADMIN\MZou <marcus.zou@gmail.com>
2023-10-26 21:53:24 +0200

    nike-shop-reactjs-v1

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Button from "./Button";
+import ShoeCard from "./ShoeCard";
+import ProductCard from "./ProductCard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
+
+export { Button, ShoeCard, ProductCard, ServiceCard, ReviewCard };
